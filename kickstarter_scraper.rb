dde140c68c68b28620d95b2fb32d6d0550e53d10
Amgad Malik <amgadmalik@yahoo.com>
2019-10-21 02:33:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,58 +12,25 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+ 
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+ 
+  # return the projects hash
+  projects
+
   
 
 end
 
 create_project_hash
-
-
-# require 'open-uri'
-# require 'pry'
-
-# require_relative './course.rb'
-
-# class Scraper
-  
-#   def get_page
- 
-#     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
- 
-#     # doc.css(".post").each do |post|
-#     #   course = Course.new
-#     #   course.title = post.css("h2").text
-#     #   course.schedule = post.css(".date").text
-#     #   course.description = post.css("p").text
-#     #end
-#   end
-  
-#   def get_courses
-#     self.get_page.css(".post")
-#   end 
-  
-#   def make_courses
-#   self.get_courses.each do |post|
-#     course = Course.new
-#     course.title = post.css("h2").text
-#     course.schedule = post.css(".date").text
-#     course.description = post.css("p").text
-#     end
-#   end 
-
-#   def print_courses
-#     self.make_courses
-#     Course.all.each do |course|
-#       if course.title && course.title != ""
-#         puts "Title: #{course.title}"
-#         puts "  Schedule: #{course.schedule}"
-#         puts "  Description: #{course.description}"
-#       end
-#     end
-#   end
-  
-# end
-
-# Scraper.new.print_courses
 
